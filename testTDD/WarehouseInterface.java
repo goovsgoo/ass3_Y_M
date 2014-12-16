67d3e7f3afee1f021b56df2eddfe44dbf7ed380f
‘Goo’ <yoedgrizim@gmail.com>
2014-12-16 22:40:18 +0200

    12312

---
+++ testTDD/WarehouseInterface.java
@@ -0,0 +1,27 @@
+package REIT.testing; 
+
+/*
+ *
+2.6 Warehouse
+Found in: Management, RunnableMaintainenceRequest
+
+This object contains: (1) Collection of repair tools (2) Collection of repair materials.
+
+This warehouse is the shared storage component, where the different maintenance persons acquire
+their tools and materials. You will have methods that allow the maintenance person to acquire and
+release tools, as well as to consume repair material.
+Note: The warehouse contains enough maintenance materials and tools in order to successfully
+complete the simulation process. There is no case where your simulation will get stuck due to insufficient units.
+ */
+
+
+public interface WarehouseInterface {
+
+	
+	//maintenance man asking to take stuff that needed for a repair.
+	public void acquireTool(RepairRequirements repairRequirements);
+	
+	//maintenance man returning the stuff used for repair.
+	public void releaseTool(RepairRequirements repairRequirements);
+
+}
