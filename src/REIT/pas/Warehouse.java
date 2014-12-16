44ed39e7e12918a13740c30707f1e7527e017c1a
‘Goo’ <yoedgrizim@gmail.com>
2014-12-17 00:33:11 +0200

    Warehouse in Progress by yoed

--- src/REIT/pas/Warehouse.java
+++ src/REIT/pas/Warehouse.java
@@ -1,37 +1,115 @@
-package REIT.testing;
+package REIT.pas;
 
 import java.util.ArrayList;
-
-import REIT.passives.RepairRequirements;
-import REIT.passives.RepairMaterial;
-import REIT.passives.RepairTool;
-import REIT.passives.WarehouseInterface;
 
 /**
  * 
  * @author Meni & Yoed
- * the Warehouse class simulates the REIT's maintenance storage.
+ * Warehouse class implement the REIT storage
  * 
  * it holds two databases:
- * 		ArrayList<RepairTool> - list of tools being implemented with semaphores. 
- * 		ArrayList<RepairMaterial> - list of materials.
+ * 		ArrayList<RepairTool> - list of tools being implemented with semaphores.
+ * 		ArrayList<RepairMaterial> - list of materials
  *  
  * the class offers two public methods:
- * 		acquire(RepairRequirements repairRequirements) - for all tools and materials in the repairRequirements, calls for the tools' acquire() method and subtracts the material' quantity.
- * 		release(RepairRequirements repairRequirements) - for all tools and materials in Dish, calls for the tools' release().
+ * 		acquire(RepairRequirements repairRequirements) - for all tools and materials in the repairRequirements, calls for the tools' acquire() method and subtracts the material' quantity
+ * 		release(RepairRequirements repairRequirements) - for all tools and materials in Dish, calls for the tools' release()
  * 
  */
+
+
 public class Warehouse implements WarehouseInterface {
 	
 	private ArrayList<RepairTool> tools;
-	private ArrayList<RepairMaterial> materials;
+	private ArrayList<RepairMaterial> RepairMaterials;
+	
+	private static Warehouse SAMPLE = null;
 
 	
-	public Warehouse(){
+	private Warehouse(){
 		tools = new ArrayList<RepairTool>();
-		materials = new ArrayList<RepairMaterial>();
+		RepairMaterials = new ArrayList<RepairMaterial>();
 	}
 	
+	static public Warehouse sample(){
+		if (SAMPLE == null)
+			SAMPLE = new Warehouse();
+		return SAMPLE;
+	}
+	
+	
+	/**
+	 * this method simulates a maintenance man asking to take what needed for a certain asset repair.
+	 * the method calls for the proper tools and do acquire() method.
+	 * @param repairRequirements of the tools and materials needed to be acquired.
+	 */
+	public void acquire(RepairRequirements repairRequirements) {
+		// acquire RepairTool
+		for (int i = 0 ; i < tools.size() ; i++){
+			RepairTool tool = tools.get(i);
+			int quantity = repairRequirements.isRequired(tool);
+			if (quantity > 0)
+				tool.acquire(quantity);
+		}																// Yoed stop here
+		// acquire materials
+		for (RepairMaterial ingredient : ingredients){
+			int quantity = RepairRequirements.isRequired(ingredient);
+			if (quantity > 0){
+				ingredient.acquire(quantity);
+			}
+		}
+	}
+
+	/**
+	 * this method simulates a chef returning the tools used to prepare a dish.
+	 * the method calls for the appropriate tools' release() method.
+	 * @param dish which order was finished and it's tools are released.
+	 */
+	public void release(Dish dish) {
+		for (int i = tools.size()-1 ; i >= 0 ; i--){
+			KitchenTool tool = tools.get(i);
+			int quantity = dish.isRequired(tool);
+			if (quantity > 0)
+				tool.release(quantity);
+		}
+	}
+
+	/**
+	 * this method adds a new KitchenTool to the Warehouse.
+	 * @param tool to be added.
+	 */
+	protected void addTool(KitchenTool newTool){
+		for (KitchenTool tool : tools){
+			if (tool.equals(newTool)){
+				tool.release(newTool.quantity());
+				return;
+			}
+		}
+		tools.add(newTool);
+	}
+	
+	/**
+	 * this method adds a new ingredient to the Warehouse.
+	 * @param ingredient to be added.
+	 */
+	protected void addIngredient(Ingredient newIngredient){
+		for (Ingredient ingredient : ingredients){
+			if (ingredient.equals(newIngredient)){
+				ingredient.add(newIngredient);
+				return;
+			}
+		}
+		ingredients.add(newIngredient);
+	}
+
+
+}
+
+
+
+
+
+
 	/**
 	 * this method simulates a maintenance man asking to take what needed for a certain asset repair.
 	 * the method calls for the appropriate tools' acquire() method.
