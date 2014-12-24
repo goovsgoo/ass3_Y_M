ed866bb63560b8f708842d5b600ed6de5d8bd9ed
meni <menitm@gmail.com>
2014-12-24 14:38:40 +0200

    rental request

---
+++ src/REIT/pas/RentalRequest.java
@@ -0,0 +1,39 @@
+package REIT.pas;
+
+public class RentalRequest implements Comparable<RentalRequest>{
+	private final String ID;
+	private final String assetType;
+	private final int assetSize;
+	private final int stay;
+	private Asset assetLinked;
+	private String status;
+	
+	public RentalRequest(String id, String type, int size, int duration) {
+		this.ID = id;
+		this.assetType = type;
+		this.assetSize = size;
+		this.stay = duration;
+		this.assetLinked = null;
+		this.status = "incomplete";
+	}
+	
+	public void updateStatus(String newStatus) {
+		this.status = newStatus;
+	}
+	
+	public void LinkAsset(Asset assetFound) {
+		this.assetLinked = assetFound;
+	}
+	
+	// Overriding the compareTo method
+	public int compareTo(RentalRequest other){
+		return this.assetSize - other.assetSize;
+	}
+	   	   
+	/**
+	 * for overrides toString method  
+	 */
+	public String toString(){
+		return this.ID;
+	}
+}
